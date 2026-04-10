debb996280e7fdb6775296c34adeb13229bd4021
Mallhar Totey <mallhartotey2903@gmail.com>
2026-04-10 16:59:42 +0530

    The main code file
    Removed commented lines for clarity and consistency.

---
+++ Task1/GPT2.py
@@ -0,0 +1,262 @@
+import torch
+import torch.nn as nn
+from torch.nn import functional as F
+import DataLoader #importing the provided DataLoader
+from tokenizers import Tokenizer   #front hugging face importing the tokenizers library
+
+
+# hyperparameters
+batch_size = 64 # represents how many independent sequences will we process in parallel?
+block_size = 256 # maximum context length for predictions this is the time dimension
+total_iters = 1000  
+eval_interval = 50
+learning_rate = 1e-4
+device = 'cuda' if torch.cuda.is_available() else 'cpu'
+eval_iters = 200
+n_embd = 384
+n_head = 6
+n_layer = 6
+weight_decay = 1e-1
+dropout = 0.2
+# ------------
+
+torch.manual_seed(7)
+#DataLoader.download_dataset()
+text = DataLoader.load_dataset()
+
+
+#getting the BPE tokenizer that we just trained
+tokenizer = Tokenizer.from_file("Shakespeare_tokenizer.json")
+vocab_size = tokenizer.get_vocab_size()
+#encode and decoder for performing the tokenizer
+encode = lambda s : tokenizer.encode(s).ids
+decode = lambda l : tokenizer.decode(l)
+
+# Train and validation splits
+data = torch.tensor(encode(text), dtype=torch.long)
+n = int(0.9*len(data)) # first 90% will be train, rest val
+train_data = data[:n]
+val_data = data[n:]
+
+# data loading
+def get_batch(split):
+    """adding a doc_string"""
+    data = train_data if split == 'train' else val_data
+    ix = torch.randint(len(data) - block_size, (batch_size,))
+    x = torch.stack([data[i:i+block_size] for i in ix])
+    y = torch.stack([data[i+1:i+block_size+1] for i in ix])
+    x, y = x.to(device), y.to(device)
+    return x, y
+
+@torch.no_grad()
+def estimate_loss():
+    out = {}
+    model.eval()
+    for split in ['train', 'val']:
+        losses = torch.zeros(eval_iters)
+        for k in range(eval_iters):
+            X, Y = get_batch(split)
+            logits, loss = model(X, Y)
+            losses[k] = loss.item()
+        out[split] = losses.mean()
+    model.train()
+    return out
+
+class Head(nn.Module):
+    """ one head of self-attention """
+
+    def __init__(self, head_size):
+        super().__init__()
+        self.key = nn.Linear(n_embd, head_size, bias=False)
+        self.query = nn.Linear(n_embd, head_size, bias=False)
+        self.value = nn.Linear(n_embd, head_size, bias=False)
+        self.register_buffer('tril', torch.tril(torch.ones(block_size, block_size)))
+
+        self.dropout = nn.Dropout(dropout)
+
+    def forward(self, x):
+        """Adding a doc string"""
+        B,T,C = x.shape
+        k = self.key(x)   # (B,T,hs)
+        q = self.query(x) # (B,T,hs)
+        # compute attention scores ("affinities")
+        attn_scores = q @ k.transpose(-2,-1) * k.shape[-1]**-0.5 # (B, T, hs) @ (B, hs, T) -> (B, T, T)
+        attn_scores = attn_scores.masked_fill(self.tril[:T, :T] == 0, float('-inf')) # (B, T, T)
+        attn_scores = F.softmax(attn_scores, dim=-1) # (B, T, T)
+        attn_scores = self.dropout(attn_scores)
+        # performing the weighted aggregation of the values
+        v = self.value(x) # (B,T,hs)
+        out = attn_scores @ v # (B, T, T) @ (B, T, hs) -> (B, T, hs)
+        return out
+
+class MultiHeadAttention(nn.Module):
+    """ multiple heads of self-attention in parallel """
+
+    def __init__(self, num_heads, head_size):
+        '''write your own doc string'''
+        super().__init__()
+        self.heads = nn.ModuleList([Head(head_size) for _ in range(num_heads)])
+        self.proj = nn.Linear(head_size * num_heads, n_embd)
+        self.dropout = nn.Dropout(dropout)
+
+    def forward(self, x):
+        out = torch.cat([h(x) for h in self.heads], dim=-1)
+        out = self.dropout(self.proj(out))
+        return out
+
+class FeedForward(nn.Module):
+    """ a simple linear layer followed by a non-linearity """
+
+    def __init__(self, n_embd):
+        super().__init__()
+        self.net = nn.Sequential(
+            nn.Linear(n_embd, 4 * n_embd),
+            nn.GELU(),   
+            nn.Linear(4 * n_embd, n_embd),
+            nn.Dropout(dropout),
+        )
+
+    def forward(self, x):
+        return self.net(x)
+
+class Block(nn.Module):
+    """ Transformer block: communication followed by computation """
+
+    def __init__(self, n_embd, n_head):
+        # n_embd: embedding dimension, n_head: the number of heads we'd like
+        super().__init__()
+        head_size = n_embd // n_head
+        self.sa = MultiHeadAttention(n_head, head_size)
+        self.ffwd = FeedForward(n_embd)
+        self.ln1 = nn.LayerNorm(n_embd)
+        self.ln2 = nn.LayerNorm(n_embd)
+
+    def forward(self, x):
+        x = x + self.sa(self.ln1(x))
+        x = x + self.ffwd(self.ln2(x))
+        return x
+
+class GPTLanguageModel(nn.Module):
+
+    def __init__(self):
+        super().__init__()
+        # each token directly reads off the logits for the next token from a lookup table
+        self.token_embedding_table = nn.Embedding(vocab_size, n_embd)
+        self.position_embedding_table = nn.Embedding(block_size, n_embd)
+        self.blocks = nn.Sequential(*[Block(n_embd, n_head=n_head) for _ in range(n_layer)])
+        self.ln_f = nn.LayerNorm(n_embd) # final layer norm
+        self.lm_head = nn.Linear(n_embd, vocab_size)
+
+        self.apply(self._init_weights)
+
+    def _init_weights(self, module):
+        if isinstance(module, nn.Linear):
+            torch.nn.init.normal_(module.weight, mean=0.0, std=0.02)
+            if module.bias is not None:
+                torch.nn.init.zeros_(module.bias)
+        elif isinstance(module, nn.Embedding):
+            torch.nn.init.normal_(module.weight, mean=0.0, std=0.02)
+
+    def forward(self, idx, targets=None):
+        B, T = idx.shape
+
+        # idx and targets are both (B,T) tensor of integers
+        tok_emb = self.token_embedding_table(idx) # (B,T,C)
+        pos_emb = self.position_embedding_table(torch.arange(T, device=device)) # (T,C)
+        x = tok_emb + pos_emb # (B,T,C)
+        x = self.blocks(x) # (B,T,C)
+        x = self.ln_f(x) # (B,T,C)
+        logits = self.lm_head(x) # (B,T,vocab_size)
+
+        if targets is None:
+            loss = None
+        else:
+            B, T, C = logits.shape
+            logits = logits.view(B*T, C)
+            targets = targets.view(B*T)
+            loss = F.cross_entropy(logits, targets)
+
+        return logits, loss
+
+    def generate(self, idx, max_new_tokens, temperature = 1.0):
+        # idx is (B, T) array of indices in the current context
+        for _ in range(max_new_tokens):
+            # crop idx to the last block_size tokens
+            idx_cond = idx[:, -block_size:]
+            # get the predictions
+            logits, _ = self(idx_cond)
+            # focus only on the last time step
+            logits = logits[:, -1, :] # becomes (B, C)
+            #applying temperature
+            logits = logits / temperature 
+            # apply softmax to get probabilities
+            probs = F.softmax(logits, dim=-1) # (B, C)
+            # sample from the distribution
+            idx_next = torch.multinomial(probs, num_samples=1) # (B, 1)
+            # append sampled index to the running sequence
+            idx = torch.cat((idx, idx_next), dim=1) # (B, T+1)
+        return idx
+
+model = GPTLanguageModel()
+m = model.to(device)
+# print the number of parameters in the model
+print(sum(p.numel() for p in m.parameters())/1e6, 'M parameters')
+
+# create a PyTorch optimizer
+optimizer = torch.optim.AdamW(model.parameters(), lr = learning_rate, weight_decay = weight_decay)
+
+best_val_loss = float('inf')
+
+for iter in range(total_iters):
+
+    # evaluate the loss on train and val sets
+    if iter % eval_interval == 0 or iter == total_iters - 1:
+        losses = estimate_loss()
+        print(f"step {iter}: train loss {losses['train']:.4f}, val loss {losses['val']:.4f}")
+        
+        # EARLY STOPPING: Save the model ONLY if validation loss improves
+        if losses['val'] < best_val_loss:
+            best_val_loss = losses['val']
+            torch.save(m.state_dict(), 'best_gpt_weights.pt')
+
+    # sample a batch of data
+    xb, yb = get_batch('train')
+
+    # evaluate the loss
+    logits, loss = model(xb, yb)
+    optimizer.zero_grad(set_to_none=True)
+    loss.backward()
+    optimizer.step()
+
+print(f"\n--- Training Complete. Best Val Loss: {best_val_loss:.4f} ---")
+m.load_state_dict(torch.load('best_gpt_weights.pt'))
+print("\n--- least loss model is loaded. Ready for Generation ---")
+
+# generate from the model
+while True:
+    user_prompt = input("Enter a starting prompt (or 'quit' to exit): ")
+    
+    if user_prompt.lower() == 'quit':
+        print("Exiting gpt...")
+        break
+    
+
+    #letting the temperature be set by the user
+    temp_input = input("Enter the desirable temperature ")
+    try:
+        temp_val = float(temp_input)
+    except ValueError:
+        temp_val = 1.0
+
+    # Encode the text into integers
+    input_ids = encode(user_prompt)
+    
+    context = torch.tensor(input_ids, dtype=torch.long, device=device).unsqueeze(0)
+    
+    #actual generation
+    with torch.no_grad():
+        generated_ids = m.generate(context, max_new_tokens=256, temperature = temp_val)[0].tolist()
+    
+    #final printing
+    print(f"\n{decode(generated_ids)}\n")
+    print("-" * 50)
