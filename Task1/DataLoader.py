12b2accdab4888cf9381083b6c9e52c4fd686b15
Mallhar Totey <mallhartotey2903@gmail.com>
2026-04-11 15:16:43 +0530

    Update DataLoader.py

--- Task1/DataLoader.py
+++ Task1/DataLoader.py
@@ -1,9 +1,4 @@
-"""
-This script downloads the Tiny Shakespeare dataset from URL
-and saves it locally as a text file named "shakespeare.txt".
 
-You can change the save location by modifying the DATA_PATH variable.
-"""
 import requests
 import os
 
@@ -20,20 +15,11 @@ def download_dataset()->None:
 
 
 def load_dataset(print_text = False)->str:
-    """
-    Loads the dataset from DATA_PATH into a string.
-
-    Args:
-        print_text (bool): If True, prints the first 500 characters
-                           of the dataset for preview.
-
-    Returns:
-        str: The full dataset content as a single string.
-    """
+    
     with open(DATA_PATH, "r") as f:
         txt = f.read()
 
-    print("Total Characters in text: ", len(txt))
+#    print("Total Characters in text: ", len(txt))
     if print_text == True:
         print(txt[:500]) 
 
