ec1abb043348304293444093e3adfadf458d1c05
Mallhar Totey <mallhartotey2903@gmail.com>
2026-04-11 23:22:03 +0530

    Update GPT2.py

--- Task1/GPT2.py
+++ Task1/GPT2.py
@@ -40,7 +40,6 @@ val_data = data[n:]
 
 # data loading
 def get_batch(split):
-    """adding a doc_string"""
     data = train_data if split == 'train' else val_data
     ix = torch.randint(len(data) - block_size, (batch_size,))
     x = torch.stack([data[i:i+block_size] for i in ix])
@@ -105,7 +104,7 @@ class MultiHeadAttention(nn.Module):
         return out
 
 class FeedForward(nn.Module):
-    """ a simple linear layer followed by a non-linearity """
+    """ a simple linear layer followed by a non-linearity (GELU)"""
 
     def __init__(self, n_embd):
         super().__init__()
